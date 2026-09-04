e1b3e8e75b726fe28cdd3277e18e59c7ec7edd38
zhuravelta963-lang <zhuravelta963@gmail.com>
2026-09-04 12:02:23 +0300

    Add files via upload

---
+++ pair2/index.js
@@ -0,0 +1,102 @@
+//< > >= <= == === !== !=
+// let a, b;
+// a = "1";
+// b = 1;
+// console.log(a === b);
+
+// if (умова) {
+//     код
+// }
+
+// let temp = Number(prompt("Enter a temperature"));
+// let result;
+// if (temp < 0){
+//     result = "Cold;"
+// }
+// else if(temp >= 25){
+//     result = "Hot";
+// }
+// else if(temp > 0 && temp < 24){
+//     result = "Norm"
+// }
+// alert(result);
+
+
+//!!!!!!! TASK !!!!!!!-------------------
+
+// let num = Number(prompt("Enter a number"));
+// if (num % 2 === 0) {
+//     alert("парне");
+// }
+// else{
+//     alert("непарне");
+// }
+//-----------------------------------------
+
+// const login = "admin";
+// const password = 12345;
+//
+// let user_login = prompt("Enter your username");
+// let user_password = prompt("Enter your password");
+//
+// if (user_login === login && user_password === password) {
+//     alert("Access is allowed");
+// }
+// else{
+//     alert("Access is forbiden");
+// }
+
+
+// post 100
+// courier 200
+// pickup 0
+
+// let deliveryType = prompt('Please enter a delivery type');
+// let cost = 0;
+// switch (deliveryType) {
+//     case 'post':
+//         cost = 100;
+//         break;
+//     case 'courier':
+//         cost = 200;
+//         break;
+//     case 'pickup':
+//         cost = 0;
+//         break;
+//     default:
+//         cost = "no answer";
+// }
+// alert(cost);
+
+
+
+//------------------------------------------------------------
+//!!!!!!TASK--------------------------------------------------
+// let num = Number(prompt("Enter a number of a week day"));
+// switch(num) {
+//     case "1":
+//         alert("Monday");
+//         break;
+//     case '2':
+//         alert("Tuesday");
+//         break;
+//     case '3':
+//         alert("Wednesday");
+//         break;
+//     case '4':
+//         alert("Thursday");
+//         break;
+//     case '5':
+//         alert("Friday");
+//         break;
+//     case '6':
+//         alert("Saturday");
+//         break;
+//     case '7':
+//         alert("Sunday");
+//         break;
+//     default:
+//         alert("A valid number");
+// }
+//
+// ------------------------------------
