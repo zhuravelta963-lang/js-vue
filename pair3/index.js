52df5beceec86f50b3e2263c4a91ac499b6c4bcd
zhuravelta963-lang <zhuravelta963@gmail.com>
2026-09-09 11:13:07 +0300

    Add files via upload

---
+++ pair3/index.js
@@ -0,0 +1,84 @@
+// let age = prompt("What is your age?");
+// let access = confirm("Are tou registered?");
+// if(age >= 18 && access === true){
+//     alert("Welcome");
+// }
+// else{
+//     alert("Access denied");
+// }
+
+
+// let login = confirm("Are you registered?");
+// let age = Number(prompt("What is your age?"));
+// if (login === true){
+//     if(age >= 18){
+//         alert("Access allowed");
+//     }
+//     else{
+//         alert("Access denied");
+//     }
+// }
+// else{
+//     alert("Not logged in");
+// }
+
+// let role = prompt("What is your role?");
+// if (role === "admin" || role === "teacher") {
+//     alert("Welcome");
+// }
+// else{
+//     alert("Denied");
+// }
+
+
+let age = Number(prompt("What is your age?"));
+// if (age < 6){
+//     alert("Дитина");
+// }
+// else if (age >= 6 && age <= 13){
+//     alert("Школяр");
+// } else if(age >= 14 && age <= 18){
+//     alert("Підліток");
+// } else{
+//     alert("Дорослий");
+// }
+
+
+// if(age >= 18){
+//     alert("Adult");
+// } else if(age <= 14) {
+//     alert("Teen");
+// } else if(age <= 18) {
+//     alert("Adult");
+// }
+// else{
+//     alert("Expired")
+// }
+
+// let age = prompt("What is your age?");
+// if (age >= 18){
+//     alert("Access granted");
+// } else if(age < 18 ) {
+//     let access = confirm("Access?");
+//     if (access) {
+//         alert("Access granted");
+//     }
+//     else{
+//         alert("lox");
+//     }
+// }
+
+// let name = prompt('NAME');
+// let price = prompt("price");
+// let quantity = prompt("Quantity");
+// let sign = confirm("Are tou signed in");
+// let promo = prompt("Have promo?");
+// const promo2 = "sale";
+// let VIP = confirm("status");
+// const sale1 = 0.1;
+
+// if(price*quantity >= 1000 && sign === true && (promo === promo2 || VIP === true)){
+//     alert(`Price with sale:${price*quantity*sale1}`)
+// }else{
+//     alert("tralalal")
+// }
